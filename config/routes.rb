3b9f96b39a214f76623c8a92853ec31ec4eb4d37
EC2 Default User <ec2-user@ip-172-31-26-159.ec2.internal>
2022-03-16 14:58:57 +0000

    make new,index,show,edit(not doing)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+  get 'books/new' => 'books#new'
   root :to => 'homes#top'
+  post 'books' => 'books#create'
+  get 'books/index' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
