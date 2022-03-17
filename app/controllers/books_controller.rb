3cd3125807c363a76e1048de85d690f822482615
EC2 Default User <ec2-user@ip-172-31-26-159.ec2.internal>
2022-03-17 11:45:39 +0000

    fixed 新規投稿保存

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,16 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     book = Book.new(book_params)
     book.save
     redirect_to book_path(book.id)
+
+
   end
 
   def index
     @book = Book.all
+    @book_new = Book.new
   end
 
   def show
